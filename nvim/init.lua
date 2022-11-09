7edb813b0ec7a506222913b2cd182013b06d917c
Rail Hamdeev <rail@hamdeew.ru>
2022-11-09 10:38:56 +0400

    change deprecated command

--- nvim/init.lua
+++ nvim/init.lua
@@ -231,7 +231,7 @@ end
 
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
 local servers = { 'solargraph', 'intelephense' }
